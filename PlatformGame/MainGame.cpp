061c2d66d5600e844e5fb2db0f07632c77171e23
mspaprika <arunepaprika@gmail.com>
2023-09-20 11:07:51 +0100

    animation bug fixed!

---
+++ PlatformGame/MainGame.cpp
@@ -0,0 +1,47 @@
+#define PLAY_IMPLEMENTATION
+#define PLAY_USING_GAMEOBJECT_MANAGER
+#include "Play.h"
+
+constexpr int DISPLAY_WIDTH = 1280;
+constexpr int DISPLAY_HEIGHT = 720;
+constexpr int DISPLAY_SCALE = 1;
+
+void Draw();
+
+// The entry point for a PlayBuffer program
+void MainGameEntry( PLAY_IGNORE_COMMAND_LINE )
+{
+	Play::CreateManager( DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_SCALE );
+	Play::CentreAllSpriteOrigins();
+	Play::LoadBackground("Data//Backgrounds//background.png");	
+}
+
+// Called by PlayBuffer every frame (60 times a second!)
+bool MainGameUpdate( float elapsedTime )
+{
+	Draw();
+
+	return Play::KeyDown( VK_ESCAPE );
+}
+
+
+// Gets called once when the player quits the game 
+int MainGameExit( void )
+{
+	Play::DestroyManager();
+	return PLAY_OK;
+}
+
+void Draw()
+{
+	Play::ClearDrawingBuffer(Play::cWhite);
+	Play::DrawBackground();
+	Play::PresentDrawingBuffer();
+}
+
+
+
+
+
+
+
